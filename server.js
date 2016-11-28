e8c142092eaed18ebf6536342316e5d5028a4e32
Karla Tirado <ktirado@sunset.com.mx>
2016-11-28 17:25:16 -0500

    Cambios

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 'use strict';
+const express = require('express');
+const app = express();
+const chatCat = require('./app');
 
-console.log("Welcome to ChatCat");
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use('/', chatCat.router);
+
+app.listen(app.get('port'),()=>{
+	console.log('ChatCAT Running on Port: ', app.get('port'));
+});
